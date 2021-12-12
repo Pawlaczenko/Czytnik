10d81d18790ecae362ac9fef1abacf65b50cb1f9
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2021-12-12 18:25:50 +0100

    Database setup

--- Czytnik/Startup.cs
+++ Czytnik/Startup.cs
@@ -1,6 +1,8 @@
+using Czytnik.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace Czytnik
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(
+                config => config.UseSqlServer(Configuration.GetConnectionString("Application"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,5 +59,6 @@ namespace Czytnik
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
     }
 }
