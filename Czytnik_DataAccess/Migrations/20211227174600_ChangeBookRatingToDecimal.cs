cb4885bf6bbf2913dde3fb88f24461a8a67c6198
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2021-12-30 18:03:49 +0100

    Migrations

---
+++ Czytnik_DataAccess/Migrations/20211227174600_ChangeBookRatingToDecimal.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Czytnik_DataAccess.Migrations
+{
+    public partial class ChangeBookRatingToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "Books",
+                type: "decimal(2,2)",
+                nullable: true,
+                oldClrType: typeof(short),
+                oldType: "smallint",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "Rating",
+                table: "Books",
+                type: "smallint",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,2)",
+                oldNullable: true);
+        }
+    }
+}
