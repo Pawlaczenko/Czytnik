9c4c6a697645a2ab5d3e7a23f633f2c6e0fd7338
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-01-07 23:31:07 +0100

    Add Categories handling: Service, Controller, Model, ViewModel, View, JavaScript srcipt for generating layout. Change footer styling

---
+++ Czytnik/wwwroot/js/generateAlphabet.js
@@ -0,0 +1,67 @@
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const alphabetNavigation = document.getElementById("alphabetNavigation");
+const categoriesListContainer = document.getElementById("categoriesList");
+
+[...ALPHABET].forEach(letter => alphabetNavigation.innerHTML += (`<a href="#${letter}">${letter}</a>`));
+
+const generateAlphabet = (data) => {
+    let letters = data.map(category => category.category.name[0]);
+    return letters.filter((element, index) => letters.indexOf(element) === index);
+}
+
+const createCategoryBoxElement = (letter, categoriesByLetter) => {
+    let listItem = document.createElement("li");
+    listItem.id = letter;
+    listItem.className = "categories__list-item categoryBox";
+
+    let itemHeader = document.createElement("h3");
+    itemHeader.className = "categoryBox__header";
+    itemHeader.textContent = letter;
+
+    let itemList = document.createElement("ul");
+    itemList.className = "categoryBox__list";
+
+    categoriesByLetter.map(category => {
+        let itemCategory = document.createElement("li");
+        itemCategory.className = "categoryBox__list-item";
+
+        let itemAnchor = document.createElement("a");
+        itemAnchor.setAttribute("href", `/Search?categoryId=${category.category.id}`); //TODO: zrobiæ dzia³aj¹cy link
+        itemAnchor.className = "categoryBox__list-link";
+        itemAnchor.textContent = `${category.category.name} (${category.bookCount})`;
+
+        itemCategory.appendChild(itemAnchor);
+        itemList.appendChild(itemCategory);
+    });
+
+    listItem.appendChild(itemHeader);
+    listItem.appendChild(itemList);
+    return listItem;
+}
+
+const generateLayout = (data) => {
+    let letters = generateAlphabet(data);
+    letters.map(letter => {
+        let categoriesByLetter = data.filter(category => category.category.name[0] === letter);
+        let listItem = createCategoryBoxElement(letter, categoriesByLetter);
+        
+        categoriesListContainer.appendChild(listItem);
+    });
+}
+
+ function getCategoriesData() {
+    $.ajax({
+        type: 'GET',
+        url: 'Categories/GetAllCategories',
+        dataType: 'json',
+        success: function (categories) {
+            generateLayout(categories);
+        },
+        error: function (emp) {
+            alert('error');
+        }
+    });
+}
+
+getCategoriesData();
