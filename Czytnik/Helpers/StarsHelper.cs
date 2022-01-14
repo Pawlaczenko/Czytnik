eaa4e1256e37cd38e33b4f80e8fd10d1674929c8
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-01-14 23:51:39 +0100

    Change Start Rating Taghelper to support decimal rating

--- Czytnik/Helpers/StarsHelper.cs
+++ Czytnik/Helpers/StarsHelper.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Globalization;
 
 namespace Czytnik.Helpers
 {
     [HtmlTargetElement("stars")]
     public class StarsHelper : TagHelper
     {
-        public int starsCount { get; set; }
-        public string parentClass { get; set; }
+        public double Rating { get; set; }
+        public string ParentClass { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            var className = $"stars-rating stars-rating--{starsCount} {parentClass}";
+            var className = $"stars-rating {ParentClass}";
             output.TagName = "div";
+            output.Attributes.SetAttribute("style", $"--rating: {Rating.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}");
             output.Attributes.Add("class", className);
         }
     }
