1ac5a0e2b5555c65cf11c00ac84cc25565873973
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-01-14 23:30:00 +0100

    Add Stars Rating Taghelper

---
+++ Czytnik/Helpers/StarsHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Czytnik.Helpers
+{
+    [HtmlTargetElement("stars")]
+    public class StarsHelper : TagHelper
+    {
+        public int starsCount { get; set; }
+        public string parentClass { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var className = $"stars-rating stars-rating--{starsCount} {parentClass}";
+            output.TagName = "div";
+            output.Attributes.Add("class", className);
+        }
+    }
+}
