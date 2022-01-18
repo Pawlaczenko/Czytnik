c7715a9f998c0a559f1a0bea6bc98a62ae40b7ab
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2022-01-18 22:46:37 +0100

    Loading languages and categories to filter in search page

--- Czytnik/Controllers/SearchController.cs
+++ Czytnik/Controllers/SearchController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Czytnik.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using System.Threading.Tasks;
 
 namespace Czytnik.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        private readonly ILanguageService _languageService;
+        public SearchController(ICategoryService categoryService, ILanguageService languageService)
         {
-            return View();
+            _categoryService = categoryService;
+            _languageService = languageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            dynamic filteringData = new ExpandoObject();
+
+            filteringData.categories = await _categoryService.GetAll();
+            filteringData.languages = await _languageService.GetAll();
+            return View(filteringData);
         }
     }
 }
