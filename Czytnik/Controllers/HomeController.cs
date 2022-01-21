0f0c200fbafd91857ea11d0ed2dbb093bebbb156
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-01-21 15:22:22 +0100

    Add Error handling

--- Czytnik/Controllers/HomeController.cs
+++ Czytnik/Controllers/HomeController.cs
@@ -42,7 +42,7 @@ namespace Czytnik.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int code)
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
