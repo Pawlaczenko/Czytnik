4d4abcd76d95f1d0acc807856d5f6969212d8c58
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2022-01-27 22:42:35 +0100

    Add cart backend functionalities
    Render cart items
    Calculate prices and promotions
    Deleting items
    Handle changing quantity of items

---
+++ Czytnik/Services/CartService.cs
@@ -0,0 +1,58 @@
+﻿using Czytnik_DataAccess.Database;
+using Czytnik_Model.Models;
+using Czytnik_Model.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Czytnik.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly AppDbContext _dbContext;
+        public CartService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<CartItemsViewModel>> GetCartItems(int userId)
+        {
+            var itemsQuery = _dbContext.CartItems.Where(i => i.UserId == userId).Select(i => new CartItemsViewModel
+            {
+                bookId = i.BookId,
+                userId = i.UserId,
+                Title = i.Book.Title,
+                Price = i.Book.Price,
+                FullPrice = i.Book.Price * i.Quantity,
+                Cover = i.Book.Cover,
+                Quantity = i.Quantity,
+                Authors = i.Book.BookAuthors.Select(ba => $"{ba.Author.FirstName} {ba.Author.SecondName} {ba.Author.Surname}").ToList(),
+                Discount = i.Book.BookDiscounts.Where(entry => entry.BookId == i.BookId).Select(entry => entry.Discount).FirstOrDefault(),
+            });
+
+            IEnumerable<CartItemsViewModel> result = await itemsQuery.ToListAsync();
+
+            foreach (var item in result)
+            {
+                item.CalculatedPrice = (item.Discount == null) ? item.Price : CalculateDiscount(item.Price, item.Discount.DiscountValue);
+            }
+
+            return result;
+        }
+
+        public void DeleteCartItem(int bookId, int userId)
+        {
+            var cartItem = _dbContext.CartItems.Where(i => i.BookId == bookId && i.UserId == userId).First();
+            _dbContext.CartItems.Remove(cartItem);
+            _dbContext.SaveChangesAsync();
+        }
+
+        private decimal CalculateDiscount(decimal price, int discount)
+        {
+            var discountPercentage = ((decimal)discount / 100);
+            return Math.Round(price * discountPercentage, 2);
+        }
+    }
+}
