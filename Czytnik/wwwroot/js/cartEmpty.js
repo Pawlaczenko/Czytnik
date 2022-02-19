526033165798106bb6a2e113b22dcce67bdc6440
Andrzej Nawalaniec <andrzejnawalaniecp12@gmail.com>
2022-02-19 21:05:01 +0100

    Fix empty cart

--- Czytnik/wwwroot/js/cartEmpty.js
+++ Czytnik/wwwroot/js/cartEmpty.js
@@ -4,7 +4,7 @@
   const isUserLogged = document.querySelector('.js-navigation-cart').dataset.logged;
   if (isUserLogged != 'True') {
     const items = JSON.parse(myStorage.getItem('cartItems'));
-    if (items.length == 0) {
+    if (items == null || items.length == 0) {
       window.location.href = '/Cart/Empty';
       return;
     }
